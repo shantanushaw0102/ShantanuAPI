fb8b3d176916b7171d1335538c25b2058c77789c
shantanu <shantanushaw0102@gmail.com>
2023-10-29 11:05:32 +0530

    modified

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require ("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors({
+  origin:"http://localhost:3000",
+}))
 
 const connectDB = require("./db/connect")
 
